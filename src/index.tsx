d2aaa35336e21cbb2bee3db56f8eda3aa3ff33cc
Dorotea Dragojević <d.dragojevic2510@gmail.com>
2025-06-15 12:24:15 +0200

    routing fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router } from "react-router-dom";
 
 // Ensure that the root element is correctly typed as HTMLDivElement
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
