8aa7b487a1177f6569bdf0218c2382133af28b0b
Dorotea Dragojević <d.dragojevic2510@gmail.com>
2025-06-15 17:55:31 +0200

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -27,41 +27,41 @@ import OrderReviews2 from "./components/OrderReviews2";
 import { useAuth } from "./components/AuthContext";
 
 function App() {
-  const { user } = useAuth();
+  const { user, isAuthenticated } = useAuth();
   return (
     <Routes>
 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route path="/fyp" element={user.isAuthenticated && user.role === 'user' ? <ForYouPage /> : <Navigate to="/login"/>}/> 
-      <Route path="/" element={user.isAuthenticated && user.role === 'user' ? <Home /> : <View/>}/> 
+      <Route path="/fyp" element={isAuthenticated && user.role === 'user' ? <ForYouPage /> : <Navigate to="/login"/>}/> 
+      <Route path="/" element={isAuthenticated && user.role === 'user' ? <Home /> : <View/>}/> 
       <Route path="/product" element={<ProductDetail /> }/> 
-      <Route path="/cart" element={user.isAuthenticated && user.role === 'user' ? <Cart /> : <Navigate to="/login"/>}/> 
-      <Route path="/checkout" element={user.isAuthenticated && user.role === 'user' ? <Checkout /> : <Navigate to="/login"/>}/>
-      <Route path="/review" element={user.isAuthenticated && user.role === 'user' ? <Review /> : <Navigate to="/login"/>}/>
+      <Route path="/cart" element={isAuthenticated && user.role === 'user' ? <Cart /> : <Navigate to="/login"/>}/> 
+      <Route path="/checkout" element={isAuthenticated && user.role === 'user' ? <Checkout /> : <Navigate to="/login"/>}/>
+      <Route path="/review" element={isAuthenticated && user.role === 'user' ? <Review /> : <Navigate to="/login"/>}/>
 
 
-      <Route path="/orders" element={user.isAuthenticated ? <Orders /> : <Navigate to="/login"/>}/> 
-      <Route path="/order/:id" element={user.isAuthenticated ? <OrderDetails /> : <Navigate to="/login"/>}/> 
+      <Route path="/orders" element={isAuthenticated ? <Orders /> : <Navigate to="/login"/>}/> 
+      <Route path="/order/:id" element={isAuthenticated ? <OrderDetails /> : <Navigate to="/login"/>}/> 
 
 
       <Route
         path="/admin"
-        element={user.isAuthenticated && user.role === 'admin' ? <AdminPage /> : <Navigate to="/login" />}
+        element={isAuthenticated && user.role === 'admin' ? <AdminPage /> : <Navigate to="/login" />}
       />
-      <Route path="/add-product" element={user.isAuthenticated && user.role === 'admin' ? <CreateProduct /> : <Navigate to="/login"/>}/>
-      <Route path="/product-admin" element={user.isAuthenticated && user.role === 'admin' ? <UpdateProduct /> : <Navigate to="/login"/>}/>
-      <Route path="/categories" element={user.isAuthenticated && user.role === 'admin' ? <Categories /> : <Navigate to="/login"/>}/>
-      <Route path="/units" element={user.isAuthenticated && user.role === 'admin' ? <Units /> : <Navigate to="/login"/>}/>
-      <Route path="/stocks" element={user.isAuthenticated && user.role === 'admin' ? <Stocks /> : <Navigate to="/login"/>}/>
-      <Route path="/manufacturers" element={user.isAuthenticated && user.role === 'admin' ? <Manufacturers /> : <Navigate to="/login"/>}/>
-      <Route path="/prices" element={user.isAuthenticated && user.role === 'admin' ? <PriceList /> : <Navigate to="/login"/>}/>
-      <Route path="/coupons" element={user.isAuthenticated && user.role === 'admin' ? <Coupons /> : <Navigate to="/login"/>}/>
-      <Route path="/reviews" element={user.isAuthenticated && user.role === 'admin' ? <OrderReviews2 /> : <Navigate to="/login"/>}/>
-      <Route path="/stats" element={user.isAuthenticated && user.role === 'admin' ? <Stats /> : <Navigate to="/login"/>}/>
-      <Route path="/skladiste" element={user.isAuthenticated && user.role === 'admin' ? <Warehouse /> : <Navigate to="/login"/>}/>
-      <Route path="/FAQ" element={user.isAuthenticated && user.role === 'admin' ? <FAQ /> : <Navigate to="/login"/>}/>
+      <Route path="/add-product" element={isAuthenticated && user.role === 'admin' ? <CreateProduct /> : <Navigate to="/login"/>}/>
+      <Route path="/product-admin" element={isAuthenticated && user.role === 'admin' ? <UpdateProduct /> : <Navigate to="/login"/>}/>
+      <Route path="/categories" element={isAuthenticated && user.role === 'admin' ? <Categories /> : <Navigate to="/login"/>}/>
+      <Route path="/units" element={isAuthenticated && user.role === 'admin' ? <Units /> : <Navigate to="/login"/>}/>
+      <Route path="/stocks" element={isAuthenticated && user.role === 'admin' ? <Stocks /> : <Navigate to="/login"/>}/>
+      <Route path="/manufacturers" element={isAuthenticated && user.role === 'admin' ? <Manufacturers /> : <Navigate to="/login"/>}/>
+      <Route path="/prices" element={isAuthenticated && user.role === 'admin' ? <PriceList /> : <Navigate to="/login"/>}/>
+      <Route path="/coupons" element={isAuthenticated && user.role === 'admin' ? <Coupons /> : <Navigate to="/login"/>}/>
+      <Route path="/reviews" element={isAuthenticated && user.role === 'admin' ? <OrderReviews2 /> : <Navigate to="/login"/>}/>
+      <Route path="/stats" element={isAuthenticated && user.role === 'admin' ? <Stats /> : <Navigate to="/login"/>}/>
+      <Route path="/skladiste" element={isAuthenticated && user.role === 'admin' ? <Warehouse /> : <Navigate to="/login"/>}/>
+      <Route path="/FAQ" element={isAuthenticated && user.role === 'admin' ? <FAQ /> : <Navigate to="/login"/>}/>
 
     </Routes>
   );
